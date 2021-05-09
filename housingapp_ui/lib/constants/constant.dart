dc0177eb06b0d58102db9841741feedb8f8f288d
Mehul Anshumali <mehulanshumali.ma@gmail.com>
2021-05-09 14:25:26 +0530

    Delete housingapp_ui directory

--- housingapp_ui/lib/constants/constant.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const Color white = Colors.white;
-const Color black = Colors.black;
-const Color red = Colors.red;
-const Color darkBlue = Color.fromRGBO(19, 26, 44, 1.0);
-
-const double appPadding = 30.0;
